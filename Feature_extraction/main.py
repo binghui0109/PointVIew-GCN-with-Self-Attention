857a8030e4723a19aa9860c52809ee86408d0c73
binghui0109 <149461243+binghui0109@users.noreply.github.com>
2023-11-03 09:29:27 +0800

    Update main.py

--- Feature_extraction/main.py
+++ Feature_extraction/main.py
@@ -12,8 +12,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-bs", "--batchSize", type=int, default=1)
 parser.add_argument("-num_class", type=int, default=40)
-parser.add_argument('--val_path', default='../data/single_view_modelnet/modelnetdata/*/test', help='path of the test data')
-parser.add_argument('--train_path', default='../data/single_view_modelnet/modelnetdata/*/train', help='path of the train data')
+parser.add_argument('--val_path', default='../data/single_view_modelnet/*/test', help='path of the test data')
+parser.add_argument('--train_path', default='../data/single_view_modelnet/*/train', help='path of the train data')
 parser.add_argument('--output_data_path', default='../data/modelnet_trained_feature/', help='path of the output feature')
 parser.add_argument('--checkpoint_path', default='../log/pointnet_on_single_view.pth', help='path of the pre_trained model')
 parser.add_argument("--workers", default=0)
@@ -23,7 +23,8 @@ parser.set_defaults(train=False)
 if __name__ == '__main__':
     args = parser.parse_args()
     train_dataset = SinglePoint(args.train_path)
-    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=False,batch_size=args.batchSize) # shuffle needs to be false! it's done within the trainer
+    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=False,batch_size=args.batchSize)
+    print(train_loader) # shuffle needs to be false! it's done within the trainer
     val_dataset = SinglePoint(args.val_path)
     val_loader = torch.utils.data.DataLoader(val_dataset, shuffle=False,batch_size=args.batchSize)
 
@@ -37,23 +38,24 @@ if __name__ == '__main__':
     class_acc_test = np.zeros((args.num_class, 3))
     with torch.no_grad():
         for j, data in tqdm(enumerate(train_loader), total=len(train_loader)):
-            points = np.asarray(data[0], dtype=np.float32)
-            target1 = data[1]
-            points = points.transpose(0, 2, 1)
-            points = torch.tensor(points)
-            points, target1 = points.cuda(), target1.cuda()
-            classifier = model.eval()
-            _, _, features_train = classifier.forward(points)
             name = data[3][-1]
             name1 = os.path.split(name)[-1]
             # scene_name = name1[:-13]
             scene_name = data[2][-1]
             name2 = name1[:-4]
             out_file = args.output_data_path + str(scene_name) + '/' + 'train'
-            if not os.path.exists(out_file):
-                os.makedirs(out_file)
-
             path = os.path.join(out_file, name2 + '.pth')
+            if os.path.isfile(path):
+                continue  # Skip writing to file if it already exists   
+            points = np.asarray(data[0], dtype=np.float32)
+            target1 = data[1]
+            points = points.transpose(0, 2, 1)
+            points = torch.tensor(points)
+            points, target1 = points.cuda(), target1.cuda()
+            classifier = model.eval()
+            _, _, features_train = classifier.forward(points)            
+            if not os.path.exists(out_file):
+                os.makedirs(out_file)                        
             torch.save(features_train, path)
             vote_pool = torch.zeros(target1.shape[0], args.num_class).cuda()
             for _ in range(1):
@@ -73,6 +75,15 @@ if __name__ == '__main__':
         print('Train Instance Accuracy: %f, Class Accuracy: %f' % (instance_acc_train, class_acc_train))
 
         for j, data in tqdm(enumerate(val_loader), total=len(val_loader)):
+            name = data[3][-1]
+            name1 = os.path.split(name)[-1]
+            # scene_name = name1[:-13]
+            scene_name = data[2][-1]
+            name2 = name1[:-4]
+            out_file = args.output_data_path + str(scene_name) + '/' + 'test'
+            path = os.path.join(out_file, name2 + '.pth')
+            if os.path.isfile(path):
+                continue  # Skip writing to file if it already exists 
             points = np.asarray(data[0], dtype=np.float32)
             target1 = data[1]
             points = points.transpose(0, 2, 1)
@@ -80,17 +91,8 @@ if __name__ == '__main__':
             points, target1 = points.cuda(), target1.cuda()
             classifier = model.eval()
             _, _, features_test = classifier.forward(points)
-
-            name = data[3][-1]
-            name1 = os.path.split(name)[-1]
-            # scene_name = name1[:-13]
-            scene_name = data[2][-1]
-            name2 = name1[:-4]
-            out_file = args.output_data_path + str(scene_name) + '/' + 'test'
             if not os.path.exists(out_file):
                 os.makedirs(out_file)
-
-            path = os.path.join(out_file, name2 + '.pth')
             torch.save(features_test, path)
             vote_pool = torch.zeros(target1.shape[0], args.num_class).cuda()
             for _ in range(1):
